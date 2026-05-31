908f7264a277f157b93920f2685dbb83fb8a9ad9
Mykyta-Semerenko <mykytasemerenko369@gmail.com>
2026-05-31 22:43:05 +0300

    Add files via upload

---
+++ Praktyka_OOP_9_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Praktyka_OOP_9_1
+{
+    static void Main()
+    {
+        string s = "ввддддддддддддддііісссссссс";
+
+        int max = 1;
+        int k = 1;
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                k++;
+
+                if (k > max)
+                    max = k;
+            }
+            else
+            {
+                k = 1;
+            }
+        }
+
+        Console.WriteLine(max);
+        Console.ReadKey();
+    }
+}
